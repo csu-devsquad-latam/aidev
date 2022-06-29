9c5b2fc159262b751ef4b40c7abcdbf202962378
chyam <chyam@microsoft.com>
2022-06-29 13:41:29 +0000

    docstring

--- src/tests/train/test_train.py
+++ src/tests/train/test_train.py
@@ -3,7 +3,10 @@ A set of tests for train.py
 """
 import sys
 sys.path.append( 'src/segmentation/train/' )
+#sys.path.append( '/segmentation/train/' )
+#sys.path.append( './segmentation/train/' )
 from train import get_training_data
 
 def test_get_training_data():
+    """Assert training data is loaded."""
     assert len(get_training_data()) != 0
