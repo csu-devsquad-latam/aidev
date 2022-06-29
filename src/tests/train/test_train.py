a3816b99ad606d3375de967451f4530ad25938ec
chyam <chyam@microsoft.com>
2022-06-29 13:59:33 +0000

    from segmentation.train.train import get_training_data

--- src/tests/train/test_train.py
+++ src/tests/train/test_train.py
@@ -1,10 +1,16 @@
 """A set of tests for train.py ."""
 
-import sys
-sys.path.append( 'src/segmentation/train/' )
+#import sys
+#sys.path.append( 'src/segmentation/train/' )
 #sys.path.append( '/segmentation/train/' )
 #sys.path.append( './segmentation/train/' )
-from train import get_training_data
+#from train import get_training_data
+
+#sys.path.append('segmentation/')
+#from train.train import get_training_data
+
+#sys.path.append('./')
+from segmentation.train.train import get_training_data
 
 def test_get_training_data():
     """Assert training data is loaded."""
