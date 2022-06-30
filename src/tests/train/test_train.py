5f6b2be317f7e8d746fa17d4f7209f1687e4cdfc
chyam <chyam@microsoft.com>
2022-06-30 15:16:36 +0000

    set LOCAL in test_train.py

--- src/tests/train/test_train.py
+++ src/tests/train/test_train.py
@@ -1,17 +1,14 @@
 """A set of tests for train.py ."""
 
-#import sys
-#sys.path.append( 'src/segmentation/train/' )
-#sys.path.append( '/segmentation/train/' )
-#sys.path.append( './segmentation/train/' )
-#from train import get_training_data
-
-#sys.path.append('segmentation/')
+import sys
+sys.path.append('segmentation/')
 #from train.train import get_training_data
-
+from train import train
 #sys.path.append('./')
-from segmentation.train.train import get_training_data
+#from segmentation.train.train import get_training_data
 
 def test_get_training_data():
     """Assert training data is loaded."""
-    assert len(get_training_data()) != 0
+    train.LOCAL = True
+    #assert len(get_training_data()) != 0
+    assert len(train.get_training_data()) != 0
